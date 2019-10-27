edab1c9c231b97f87a920fc86af4d05bcbd1877a
chenhao <chenhao@unipus.cn>
2019-10-27 13:56:14 +0800

    返回静态文件

--- router/index.js
+++ router/index.js
@@ -1,52 +1,63 @@
-var express = require('express')
-var router = express.Router()
+var express = require("express");
+var router = express.Router();
+const fs = require("fs");
 
 // a middleware function with no mount path. This code is executed for every request to the router
 router.use(function(req, res, next) {
-  console.log('first', Date.now())
-  var str = '深入浅出node.js'
-  var buf = new Buffer(str, 'utf-8')
-  console.log(buf)
+  console.log("first", Date.now());
+  var str = "深入浅出node.js";
+  var buf = new Buffer(str, "utf-8");
+  console.log(buf);
 
-  next()
-})
+  next();
+});
+
+router.use("/file", function(req, res, next) {
+  fs.readFile("./static/excel/node_test.xlsx", function(error, data) {
+    console.log(error);
+    console.log(data);
+    res.setHeader("Content-Type", "application/vnd.ms-excel;charset=UTF-8");
+    res.setHeader("Content-Disposition", "attachment;filename=a.xls");
+    res.send(data);
+  });
+});
 
 // a middleware sub-stack shows request info for any type of HTTP request to the /user/:id path
 router.use(
-  '/user/:id',
+  "/user/:id",
   function(req, res, next) {
-    console.log('req.body', req.body)
-    next()
+    console.log("req.body", req.body);
+    next();
   },
   function(req, res, next) {
-    console.log('Request Type:', req.method)
-    next()
+    console.log("Request Type:", req.method);
+    next();
   }
-)
+);
 
 // a middleware sub-stack that handles GET requests to the /user/:id path
 router.get(
-  '/user/:id',
+  "/user/:id",
   function(req, res, next) {
     // if the user ID is 0, skip to the next router
-    if (req.params.id === '0') {
-      req.chenhao = 'chenhao'
-      next('route')
+    if (req.params.id === "0") {
+      req.chenhao = "chenhao";
+      next("route");
     }
     // otherwise pass control to the next middleware function in this stack
-    else next()
+    else next();
   },
   function(req, res, next) {
     // render a regular page
-    res.render('home', { title: 'id === 0', content: 'chenhao' })
+    res.render("home", { title: "id === 0", content: "chenhao" });
   }
-)
+);
 
 // handler for the /user/:id path, which renders a special page
-router.get('/user/:id', function(req, res, next) {
-  console.log('----')
-  console.log(req.chenhao)
-  res.render('home', { title: 'id === 0', content: 'wuhaha' })
-})
+router.get("/user/:id", function(req, res, next) {
+  console.log("----");
+  console.log(req.chenhao);
+  res.render("home", { title: "id === 0", content: "wuhaha" });
+});
 
-module.exports = router
+module.exports = router;
